11aa59a55dbcd912485aece2c2b21fda8f2fd188
wenkesj <samwenke@gmail.com>
2019-02-11 14:42:29 -0500

    add bridge tasks

--- gym_tool_use/__init__.py
+++ gym_tool_use/__init__.py
@@ -11,13 +11,28 @@ register(
     id='BridgeBuilding-v0',
     entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
     kwargs={'max_steps': 500, 'level': 0})
-
 register(
     id='BridgeBuilding-v1',
     entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
     kwargs={'max_steps': 500, 'level': 1})
-
 register(
     id='BridgeBuilding-v2',
     entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
     kwargs={'max_steps': 500, 'level': 2})
+
+register(
+    id='BridgeBuilding-v3',
+    entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
+    kwargs={'max_steps': 500, 'level': 3})
+register(
+    id='BridgeBuilding-v4',
+    entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
+    kwargs={'max_steps': 500, 'level': 4})
+register(
+    id='BridgeBuilding-v5',
+    entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
+    kwargs={'max_steps': 500, 'level': 5})
+register(
+    id='BridgeBuilding-v6',
+    entry_point='gym_tool_use.bridge_building:BridgeBuildingEnv',
+    kwargs={'max_steps': 500, 'level': 6})
