e97ef45e3a2581c88176b01c297d0014009ee6fc
wenkesj <samwenke@gmail.com>
2019-02-18 17:20:28 -0500

    expose info in the_plot

--- gym_tool_use/sprites.py
+++ gym_tool_use/sprites.py
@@ -31,25 +31,49 @@ class BoxSprite(prefab_sprites.MazeWalker):
         if actions == 0:    # go upward?
             if (rows + 1) < board.shape[0]:
                 if layers['P'][rows + 1, cols]: 
+                    prev = self.position
                     self._north(board, the_plot)
+                    if self.position != prev:
+                        the_plot.info['moved_box'] = 1
+                    new_rows, new_cols = self.position
+                    if layers['W'][new_rows, new_cols]:
+                        the_plot.info['moved_box_into_water'] = 1
             else:
                 self._stay(board, the_plot)
         elif actions == 1:  # go downward?
             if (rows - 1) > 0:
                 if layers['P'][rows - 1, cols]: 
+                    prev = self.position
                     self._south(board, the_plot)
+                    if self.position != prev:
+                        the_plot.info['moved_box'] = 1
+                    new_rows, new_cols = self.position
+                    if layers['W'][new_rows, new_cols]:
+                        the_plot.info['moved_box_into_water'] = 1
             else:
                 self._stay(board, the_plot)
         elif actions == 2:  # go leftward?
             if (cols + 1) < board.shape[1]:
-                if layers['P'][rows, cols + 1]: 
+                if layers['P'][rows, cols + 1]:
+                    prev = self.position
                     self._west(board, the_plot)
+                    if self.position != prev:
+                        the_plot.info['moved_box'] = 1
+                    new_rows, new_cols = self.position
+                    if layers['W'][new_rows, new_cols]:
+                        the_plot.info['moved_box_into_water'] = 1
             else:
                 self._stay(board, the_plot)
         elif actions == 3:  # go rightward?
             if (cols - 1) > 0:
                 if layers['P'][rows, cols - 1]: 
+                    prev = self.position
                     self._east(board, the_plot)
+                    if self.position != prev:
+                        the_plot.info['moved_box'] = 1
+                    new_rows, new_cols = self.position
+                    if layers['W'][new_rows, new_cols]:
+                        the_plot.info['moved_box_into_water'] = 1
             else:
                 self._stay(board, the_plot)
 
@@ -72,6 +96,7 @@ class WaterBoxSprite(BoxSprite):
         if layers['W'][rows, cols]:
             self._stay(board, the_plot)
             return
+
         super(WaterBoxSprite, self).update(
             actions, board, layers, backdrop, things, the_plot)
 
@@ -101,6 +126,7 @@ class PlayerSprite(prefab_sprites.MazeWalker):
                         if can_cross_bridge:  # cross the bridge?
                             self._teleport((self.virtual_position[0] - 2, 
                                             self.virtual_position[1] + 0))
+                            the_plot['moved_across_bridge'] = 1
                         else:
                             self._stay(board, the_plot)
                         return
@@ -115,6 +141,7 @@ class PlayerSprite(prefab_sprites.MazeWalker):
                         if can_cross_bridge:  # cross the bridge?
                             self._teleport((self.virtual_position[0] + 2, 
                                             self.virtual_position[1] + 0))
+                            the_plot['moved_across_bridge'] = 1
                         else:
                             self._stay(board, the_plot)
                         return
@@ -129,6 +156,7 @@ class PlayerSprite(prefab_sprites.MazeWalker):
                         if can_cross_bridge:  # cross the bridge?
                             self._teleport((self.virtual_position[0] + 0, 
                                             self.virtual_position[1] - 2))
+                            the_plot['moved_across_bridge'] = 1
                         else:
                             self._stay(board, the_plot)
                         return
@@ -143,6 +171,7 @@ class PlayerSprite(prefab_sprites.MazeWalker):
                         if can_cross_bridge:  # cross the bridge?
                             self._teleport((self.virtual_position[0] + 0, 
                                             self.virtual_position[1] + 2))
+                            the_plot['moved_across_bridge'] = 1
                         else:
                             self._stay(board, the_plot)
                         return
