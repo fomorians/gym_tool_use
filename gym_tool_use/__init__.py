cddab967b6d2bd2277e4418196139315a3b16395
wenkesj <samwenke@gmail.com>
2019-05-03 11:54:35 -0400

    add envs to module

--- gym_tool_use/__init__.py
+++ gym_tool_use/__init__.py
@@ -3,3 +3,11 @@ from __future__ import division
 from __future__ import print_function
 
 from gym_tool_use.trap_tube_env import BaseTrapTubeEnv, TrapTubeConfig, ACTIONS
+from gym_tool_use.transfers import (
+    PerceptualTrapTubeEnv,
+    StructuralTrapTubeEnv,
+    SymbolicTrapTubeEnv,
+    StructuralSymbolicTrapTubeEnv,
+    PerceptualStructuralTrapTubeEnv,
+    PerceptualSymbolicTrapTubeEnv,
+    PerceptualStructuralSymbolicTrapTubeEnv)
