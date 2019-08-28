c634f50dcade370afb584cbe225641c6be4c24ad
Jim Fleming <jimmy.fleming@gmail.com>
2019-08-28 14:39:59 -0700

    Add reward_threshold to environment registration

--- gym_tool_use/__init__.py
+++ gym_tool_use/__init__.py
@@ -16,45 +16,54 @@ from gym_tool_use.transfers import (
 from gym.envs.registration import register
 
 MAX_EPISODE_STEPS = 100
+REWARD_THRESHOLD = 1.0
 
 
 register(
     id="TrapTube-v0",
     entry_point="gym_tool_use.transfers:TrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="PerceptualTrapTube-v0",
     entry_point="gym_tool_use.transfers:PerceptualTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="StructuralTrapTube-v0",
     entry_point="gym_tool_use.transfers:StructuralTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="SymbolicTrapTube-v0",
     entry_point="gym_tool_use.transfers:SymbolicTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="PerceptualStructuralTrapTube-v0",
     entry_point="gym_tool_use.transfers:PerceptualStructuralTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="PerceptualSymbolicTrapTube-v0",
     entry_point="gym_tool_use.transfers:PerceptualSymbolicTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="PerceptualStructuralSymbolicTrapTube-v0",
     entry_point="gym_tool_use.transfers:PerceptualStructuralSymbolicTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
 register(
     id="StructuralSymbolicTrapTube-v0",
     entry_point="gym_tool_use.transfers:StructuralSymbolicTrapTubeEnv",
     max_episode_steps=MAX_EPISODE_STEPS,
+    reward_threshold=REWARD_THRESHOLD,
 )
